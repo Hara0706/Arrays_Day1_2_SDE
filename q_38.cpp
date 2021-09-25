7c005e48366cecf87d259f0792311b4adf5968e9
Harapriya Mohapatra <73417004+Hara0706@users.noreply.github.com>
2021-09-25 22:58:31 +0530

    Update q_38.cpp

--- q_38.cpp
+++ q_38.cpp
@@ -1,1 +1,6 @@
+// intersection point of two nodes
+// normal approach may take O(n*m) time
+// using hash table takes time O(n+m) and space O(n)
 
+// take 2 dummy nodes and find both list length and difference
+// move them simultaneously after that to reach intersection
