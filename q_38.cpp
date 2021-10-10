47860d24bbc9453cc8dd3ddb63f2e71fa46045c0
Harapriya Mohapatra <73417004+Hara0706@users.noreply.github.com>
2021-10-10 13:37:15 +0530

    Update q_38.cpp

--- q_38.cpp
+++ q_38.cpp
@@ -4,3 +4,23 @@
 
 // take 2 dummy nodes and find both list length and difference
 // move them simultaneously after that to reach intersection
+
+class Solution
+{
+    public:
+    ListNode *getIntersect(ListNode *headA, ListNode *headB)
+    {
+        if(headA == NULL || headB == NULL) return NULL;
+        
+        ListNode *a = headA;
+        ListNode *b = headB;
+        
+        while(a!=b)
+        {
+            a = a == NULL? headB:a->next;
+            b = b == NULL? headA:b->next;
+        }
+        
+        return a;
+    }
+};
